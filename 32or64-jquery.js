1806c3f7f71e26c5ff01610fd437d54505a0a1fe
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2015-11-28 13:35:13 -0500

    Complete overhaul and refactor

---
+++ 32or64-jquery.js
@@ -0,0 +1,62 @@
+//64or32.com
+
+//When the page has loaded, run the following code
+$(document).ready(function(){
+
+    //Scan the User Agent for a specific string
+    function scanUA(str) {
+        //If the string is contained in the User Agent return true
+        return window.navigator.userAgent.indexOf(str) > -1;
+    };
+
+    //set a variable for the browser's reported architecture
+    var windowUA = window.navigator.platform;
+    //define a var to be used later
+    var detected = '';
+    //if the User Agent has any of the following strings it's 64-Bit
+    if (
+            scanUA('x86_64') ||
+            scanUA('x86-64') ||
+            scanUA('Win64') ||
+            scanUA('x64;') ||
+            scanUA('amd64') ||
+            scanUA('AMD64') ||
+            scanUA('WOW64') ||
+            scanUA('x64_64') ||
+            windowUA === 'MacIntel' ||
+            windowUA === 'Linux x86_64'
+        ) {
+            detected = 64;
+    //Otherwise if it's got any of these it's a mobile device
+    } else if (
+            windowUA === 'Linux armv7l' ||
+            windowUA === 'iPad' ||
+            windowUA === 'iPhone' ||
+            windowUA === 'Android' ||
+            windowUA === 'iPod' ||
+            windowUA === 'BlackBerry'
+        ) {
+            detected = 0;
+    //This one doesn't report accurately, set it to unknown
+    } else if (
+            windowUA === 'Linux i686'
+        ) {
+            detected = -1;
+    }
+
+    //Set the className variable based on what their User Agent reported
+    var className = '';
+    if (detected === -1) {
+        className = 'unknown';
+    } else if (detected === 0) {
+        className = 'mobile';
+    } else if (detected === 64) {
+        className = 'arch64';
+    } else {
+        className = 'arch32';
+    }
+
+    //Add the class to the HTML tag at the top of the page
+    $('html').addClass(className);
+
+});
